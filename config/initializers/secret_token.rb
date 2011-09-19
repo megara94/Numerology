006c63486d37da76539c8350252d7bb55fa135ff
megara94 <linda.avendano@sv.cmu.edu>
2011-09-19 11:15:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Numerology::Application.config.secret_token = '910963e9621ce03f7bf644b36cb48f3a7a130d0268b30e8fba04b53898b21a2e0eef4e21cef6de671097eb2d38f77a7205f7427a7d867a169936253d1db98a5f'
